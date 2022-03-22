b4917baff796ba3e42eb97493b9037273fa9e7d2
Yuriy Kuptsov <msvyatoy@gmail.com>
2022-03-22 17:14:32 -0400

    Confirmation + Tracking page UI is DONE!

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,69 +2,75 @@ import React from 'react';
 import Nutritional from './Nutritional.jsx'
 import VideoDetail from './VideoDetail.jsx'
 import SupplierPage from './SupplierPage.jsx'
+import ConfirmationPage from './ConfirmationPage.jsx';
+import TrackingPage from './TrackingPage.jsx';
 import {BrowserRouter, Routes, Route, Link}  from "react-router-dom"
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/blogs" element={<VideoDetail />} />
-          <Route path="/" element={<Nutritional />}>
-          <Route index element={<SupplierPage />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-
 import axios from 'axios';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-
-    this.get = this.get.bind(this);
-    this.post = this.post.bind(this);
-  }
-
-  componentDidMount() {
-    // this.get()
-    this.post()
-  }
-
-  get() {
-    axios.get('/api')
-    .then(results => {
-      console.log('results:', results)
-    })
-
-  }
-
-  post() {
-    axios.post('/api')
-    .then(results => {
-      console.log('results:', results)
-    })
-
-  }
-
-  render() {
-    return (
-      <div>Primal Health Test</div>
-    )
-  }
-
-}
 // const App = () => {
 //   return (
-//     <div>
-//     <div>Primal Health Test</div>
-//     <Nutritional/>
-//     <VideoDetail/>
-//     <SupplierPage/>
-//     </div>
-//   )
+//     <BrowserRouter>
+//       {/* <ConfirmationPage/> */}
+//       <Routes>
+//           <Route path="/blogs" element={<VideoDetail />} />
+//           <Route path="/" element={<Nutritional />}>
+//           <Route index element={<SupplierPage />} />
+//           {/* <Route path="*" element={<NoPage />} /> */}
+//           <Route index element={<ConfirmationPage />}/>
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
 // }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {}
+
+//     this.get = this.get.bind(this);
+//     this.post = this.post.bind(this);
+//   }
+
+//   componentDidMount() {
+//     // this.get()
+//     this.post()
+//   }
+
+//   get() {
+//     axios.get('/api')
+//     .then(results => {
+//       console.log('results:', results)
+//     })
+
+//   }
+
+//   post() {
+//     axios.post('/api')
+//     .then(results => {
+//       console.log('results:', results)
+//     })
+
+//   }
+
+//   render() {
+//     return (
+//       <div>Primal Health Test</div>
+//     )
+//   }
+
+// }
+const App = () => {
+  return (
+    <div>
+    {/* <div>Primal Health Test</div> */}
+    {/* <ConfirmationPage/> */}
+    <TrackingPage/>
+    {/* <Nutritional/>
+    <VideoDetail/>
+    <SupplierPage/> */}
+    </div>
+  )
+}
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const axios = require("axios");
-const config = require("../config.js");
+// const config = require("../config.js");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.json());
 
