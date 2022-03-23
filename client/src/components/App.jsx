19873e3261f34a596a645b02b45b1a63e0c1d803
Andy Nguyen <andynguyenpb@gmail.com>
2022-03-23 10:30:06 -0400

    finished styling for shipping, and login pages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,80 +1,75 @@
 import React, { useState, useContext } from 'react';
 import axios from 'axios';
-import React, {useState} from 'react';
+import LandingPage from './LandingPage.jsx'
 import Nutritional from './Nutritional.jsx'
 import PersonalizeYourPlan from './PersonalizeYourPlan.jsx'
 import VideoDetail from './VideoDetail.jsx'
-
 import SupplierPage from './SupplierPage.jsx'
-
 import UserProfile from './UserProfile.jsx';
 import UserOrderTracking from './UserOrderTracking.jsx';
 import GoogleLogin from '../Auth/GoogleLogin.js';
 import GoogleSignUp from '../Auth/GoogleSignUp.js';
-
 import Masthead from '../shared/Masthead.jsx'
 import SelectMeals from './SelectMeals.jsx'
 import ReviewYourOrder from './ReviewYourOrder.jsx'
 import EnterCreditCard from './EnterCreditCard.jsx'
-import {BrowserRouter, Routes, Route, Link}  from "react-router-dom"
+import EnterShippingInfo from './EnterShippingInfo.jsx'
+import { BrowserRouter, Routes, Route, Link }  from "react-router-dom"
 import { AppContext } from "../context.js";
 
-const AppProvider = (props) => {
-  const [meals, setMeals] = useState([]);
-  const [selectMeals, setSelectMeals] = useState([]);
-  const [id, setId] = useState(0)
+// const AppProvider = (props) => {
+//   const [meals, setMeals] = useState([]);
+//   const [selectMeals, setSelectMeals] = useState([]);
+//   const [id, setId] = useState(0)
 
-  console.log("mealsfromapp", meals)
-  return (
-    <AppContext.Provider value= {{
-      meals, setMeals,
-      selectMeals, setSelectMeals,
-      id, setId
-      }}>
-      {props.children}
-    </AppContext.Provider>
-  )
-}
+//   console.log("mealsfromapp", meals)
+//   return (
+//     <AppContext.Provider value= {{
+//       meals, setMeals,
+//       selectMeals, setSelectMeals,
+//       id, setId
+//       }}>
+//       {props.children}
+//     </AppContext.Provider>
+//   )
+// }
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <AppProvider>
-        <Routes>
-          {/* <Route path="/order-confirmation" element={<ConfirmationPage />} /> */}
-          {/* <Route path="/credit-card-entry" element={<EnterCreditCard />} /> */}
-          {/* <Route path="/shipping" element={<EnterShippingInfo />} /> */}
-          {/* <Route path="/logged-in" element={<LoggedInOut />} /> */}
-          {/* <Route path="/health-log" element={<Logger />} /> */}
-          {/* <Route path="/login" element={<LoginPage />} /> */}
-          {/* <Route path="/recipe-menu" element={<MenuAndReview />} /> */}
-          {/* <Route path="/your-thoughts" element={<ModalWriteAReview />} /> */}
-          {/* <Route path="/nutritional-breakdown" element={<NutritionalInfoModalMenuAndReview />} /> */}
-          {/* <Route path="/recipe-card" element={<RecipeModalMenuAndReview />} /> */}
-          {/* <Route path="/review-order" element={<ReviewYourOrder />} /> */}
-          {/* <Route path="/select-meals" element={<SelectMeals />} /> */}
-          {/* <Route path="/meet-our-vendors" element={<SupplierPage />} /> */}
-          {/* <Route path="/tracking" element={<TrackingPage />} /> */}
-          {/* <Route path="/user-profile" element={<UserProfile />} /> */}
-          {/* <Route path="/user-health" element={<UserHealthLogger />} /> */}
-          {/* <Route path="/user-orders" element={<UserOrderTracking />} /> */}
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <AppProvider>
+//         <Routes>
+//           {/* <Route path="/order-confirmation" element={<ConfirmationPage />} /> */}
+//           {/* <Route path="/credit-card-entry" element={<EnterCreditCard />} /> */}
+//           {/* <Route path="/shipping" element={<EnterShippingInfo />} /> */}
+//           {/* <Route path="/logged-in" element={<LoggedInOut />} /> */}
+//           {/* <Route path="/health-log" element={<Logger />} /> */}
+//           {/* <Route path="/login" element={<LoginPage />} /> */}
+//           {/* <Route path="/recipe-menu" element={<MenuAndReview />} /> */}
+//           {/* <Route path="/your-thoughts" element={<ModalWriteAReview />} /> */}
+//           {/* <Route path="/nutritional-breakdown" element={<NutritionalInfoModalMenuAndReview />} /> */}
+//           {/* <Route path="/recipe-card" element={<RecipeModalMenuAndReview />} /> */}
+//           {/* <Route path="/review-order" element={<ReviewYourOrder />} /> */}
+//           {/* <Route path="/select-meals" element={<SelectMeals />} /> */}
+//           {/* <Route path="/meet-our-vendors" element={<SupplierPage />} /> */}
+//           {/* <Route path="/tracking" element={<TrackingPage />} /> */}
+//           {/* <Route path="/user-profile" element={<UserProfile />} /> */}
+//           {/* <Route path="/user-health" element={<UserHealthLogger />} /> */}
+//           {/* <Route path="/user-orders" element={<UserOrderTracking />} /> */}
 
-          <Route path="/your-personal-pref" element={<PersonalizeYourPlan/>} />
-          <Route path="/videos" element={<VideoDetail />} />
-          <Route path="/" element={<Nutritional />}>
-          <Route index element={<SupplierPage />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
-          </Route>
-        </Routes>
-      </AppProvider>
-    </BrowserRouter>
-  );
-// import LoginPage from './LoginPage.jsx'
-import LoginPage from './LoginPage.jsx';
-import firebase from '../firebase/firebase.js';
+//           <Route path="/your-personal-pref" element={<PersonalizeYourPlan/>} />
+//           <Route path="/videos" element={<VideoDetail />} />
+//           <Route path="/" element={<Nutritional />}>
+//           <Route index element={<SupplierPage />} />
+//           {/* <Route path="*" element={<NoPage />} /> */}
+//           </Route>
+//         </Routes>
+//       </AppProvider>
+//     </BrowserRouter>
+//   );
 
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 // import axios from 'axios';
 
@@ -220,18 +215,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <EnterCreditCard />
-      <div>
-        <div>Primal Health Test</div>
-        {this.state.userToggle
-          ? <SupplierPage suppliers={this.state.suppliers} />
-          : null
-        }
-        {/* <UserProfile /> */}
-        {/* <UserOrderTracking /> */}
-        {/* <GoogleLogin setUser={this.userLogin} /> */}
-        {/* <GoogleSignUp addUser={this.addUser} /> */}
-      </div>
+      <LandingPage />
+      // <div>
+      //   <div>Primal Health Test</div>
+      //   {this.state.userToggle
+      //     ? <SupplierPage suppliers={this.state.suppliers} />
+      //     : null
+      //   }
+      //   {/* <UserProfile /> */}
+      //   {/* <UserOrderTracking /> */}
+      // <GoogleLogin setUser={this.userLogin}/>
+      // <GoogleSignUp addUser={this.addUser} />
+      // </div>
     )
   }
 }
@@ -246,7 +241,7 @@ class App extends React.Component {
 //   )
 // }
 
-}
+
 
 export default App;
 
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,6 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  entry: ["regenerator-runtime/runtime.js", "<your enter js file>"]
+  // entry: ["regenerator-runtime/runtime.js", "<your enter js file>"]
 };
 
